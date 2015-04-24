f9f8b707ece10aa802f4bfce8d53b09e1c69ffc0
Carlos <18carloscpi@gmail.com>
2015-04-24 10:14:17 +0200

    “Script_Carlos_y_Alin”

--- imgkonvert.sh
+++ imgkonvert.sh
@@ -4,9 +4,33 @@
 # it also reduces size
 
 # Sanity checks ?
-# ...
+
+pattern='^[0-9]{3}x[0-9]{3}$'
+
+# headboard
+
+echo ""
+echo "###########################################################"
+echo "######### Photo converter (png to another format) #########"
+echo "###########################################################"
+echo ""
 
 # Main code
+
+echo -n "-Input resolution (3 digits): "
+read Resolution
+
+echo -n "-Input photo format: "
+echo ""
+echo "--------------------------------------------------------"
+echo "| Suggested formats: bmp - gif - png - jpeg - jpg -tif |"
+echo "--------------------------------------------------------"
+echo ""
+read format
+echo ""
+echo "-Conversion in progress..."
+
+if [[ "$Resolution" =~ $pattern ]]; then
 
 ls -1 $1/*.png > imagefiles
 cat imagefiles
@@ -15,11 +39,16 @@ do
 	echo "IMG file: $imgfile"
 	lengthname=${#imgfile}
 	namefile=${imgfile:0:$lengthname-4}
-	convert "$1/$imgfile" -resize 200x200 "$namefile.jpg"
+	convert "$1/$imgfile" -resize $Resolution "$namefile.$format"
 	#mogrify -resize 200x200 *.png
 	#mogrify -format png *.jpg
-		
 
 done < imagefiles
 rm imagefiles
+echo "-Done!!!"
+
+else
+echo "-Wrong resolution"
+fi
+
 exit 0
